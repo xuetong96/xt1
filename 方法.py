93b9ac3180fb40320a74c2b7f13e9fab9f398b3d
xuetong <1533442355@qq.com>
2020-04-02 20:07:50 +0800

    最新

---
+++ 方法.py
@@ -0,0 +1,61 @@
+''' 函数/方法 '''
+
+''' 方法的定义 '''
+
+'''
+def 方法的声明
+checkname 方法的名字
+username 方法的参数
+"""方法的说明"""
+方法的逻辑代码
+'''
+# def checkname(username):
+#     if len(username) >= 8 and len(username) <= 12:
+#         if username[0] in 'qwertyuiopasdfghjklzxcvbnm':
+#             print('ok')
+#         else:
+#             print('账号的首字母必须为小写字母！')
+#     else:
+#         print("密码必须8-12位！")
+
+# checkname('gydhenuubdehjdhj')
+# checkname('7ydggyhu')
+
+''' 定义一个方法：加法 '''
+# def jiafa():
+#     print(1+2)
+# jiafa()
+
+# def jiafa(a,b):
+#     print(a+b)
+
+
+# def jiafa(a,b):
+#     """
+#     这个方法的作用是实现两个数字相加
+#     """
+#     if type(a) is int and type(b) is int:
+#         print(a+b)
+#     else:
+#         print( "输入的数据类型不正确")
+
+''' 返回值 : return'''
+"""
+返回值，返回后我们可以对这个值做其他的操作
+而，print不能
+"""
+
+# def jiafa(a,b):
+#     """
+#     这个方法的作用是实现两个数字相加
+#     """
+#     if type(a) is int and type(b) is int:
+#         return(a+b)
+#     else:
+#         return'输入的数据类型不正确'
+# jiafa(2,3)
+# print('-----------')
+# print(jiafa(2,3))
+
+
+
